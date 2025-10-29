7e7426ef9c199c579dcb919443f12269c291db06
bytebytrevor <trevormngwenya@gmail.com>
2025-10-29 06:00:32 +0200

    Fix project post route

--- AllocatrApi/Program.cs
+++ AllocatrApi/Program.cs
@@ -19,18 +19,18 @@ app.MapGet("projects/{id}", (string id) => projects.Find(project => project.Id =
 // POST /projects
 app.MapPost("projects", (CreateProjectDto newProject) =>
 {
-	ProjectDto project = new (		
+	ProjectDto project = new(
 		"7f2c4b31-9d1a-4c71-b7ad-64b8e5f529d8",
 		"ALC-0011",
 		newProject.Title,
 		newProject.Description,
 		newProject.Type,
 		newProject.Category,
-		new DateTime().
+		new DateTime(),
 		newProject.UpdatedAt,
 		newProject.StartDate,
 		newProject.DueDate,
-		"pending"
+		"pending",
 		0,
 		newProject.Priority,
 		newProject.UserId,
@@ -42,11 +42,11 @@ app.MapPost("projects", (CreateProjectDto newProject) =>
 		newProject.AllowBids,
 		newProject.Budget,
 		newProject.Currency,
-		newProject.Attachments,
+		newProject.Attachments
 
-	)
+	);
 
-	projects.add(newProject)
+	projects.Add(project);
 });
 
 
