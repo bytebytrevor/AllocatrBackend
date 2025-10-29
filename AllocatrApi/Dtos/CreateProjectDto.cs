9b7017a75b569aaf9614b54cfb7740c7ab94acbb
Trevor Ngwenya <trevormngwenya@gmail.com>
2025-10-29 05:26:26 +0200

    Set up post project route

--- AllocatrApi/Dtos/CreateProjectDto.cs
+++ AllocatrApi/Dtos/CreateProjectDto.cs
@@ -1,23 +1,16 @@
 namespace AllocatrApi.Dtos;
 
 public record class CreateProjectDto(
-    string Id,
-    string ProjectCode,
     string Title,
     string Description,
     string Type,
     string Category,
-    DateTime CreatedAt,
     DateTime? UpdatedAt,
     DateOnly? StartDate,
     DateOnly? DueDate,
-    string Status,
-    int Progress,
     string? Priority,
     string UserId,
     List<ProjectMemberDto>? Allocats,
-    int? TasksCount,
-    int? MessagesCount,
     DateTime? LastActivity,
     bool IsPublic,
     bool AllowBids,
--- AllocatrApi/Program.cs
+++ AllocatrApi/Program.cs
@@ -10,13 +10,43 @@ Console.WriteLine(projects);
 
 app.MapGet("/", () => "Hello World!");
 
+// GET /projects
 app.MapGet("projects", () => projects);
 
+// GET /projects/1
 app.MapGet("projects/{id}", (string id) => projects.Find(project => project.Id == id));
 
-app.MapGet("/projects/new", (CreateProjectDto newProject) =>
+// POST /projects
+app.MapPost("projects", (CreateProjectDto newProject) =>
 {
+	ProjectDto project = new (		
+		"7f2c4b31-9d1a-4c71-b7ad-64b8e5f529d8",
+		"ALC-0011",
+		newProject.Title,
+		newProject.Description,
+		newProject.Type,
+		newProject.Category,
+		new DateTime().
+		newProject.UpdatedAt,
+		newProject.StartDate,
+		newProject.DueDate,
+		"pending"
+		0,
+		newProject.Priority,
+		newProject.UserId,
+		newProject.Allocats,
+		0,
+		0,
+		new DateTime(),
+		newProject.IsPublic,
+		newProject.AllowBids,
+		newProject.Budget,
+		newProject.Currency,
+		newProject.Attachments,
 
+	)
+
+	projects.add(newProject)
 });
 
 
