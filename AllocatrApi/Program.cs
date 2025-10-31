4017913427714b986ddd31545987ea8ce756d096
Trevor <ngw21001@byui.edu>
2025-10-31 16:56:32 +0200

    Add test data for new project creation

--- AllocatrApi/Program.cs
+++ AllocatrApi/Program.cs
@@ -19,34 +19,33 @@ app.MapGet("projects/{id}", (string id) => projects.Find(project => project.Id =
 // POST /projects
 app.MapPost("projects", (CreateProjectDto newProject) =>
 {
-	ProjectDto project = new(
-		"7f2c4b31-9d1a-4c71-b7ad-64b8e5f529d8",
-		"ALC-0011",
-		newProject.Title,
-		newProject.Description,
-		newProject.Type,
-		newProject.Category,
-		new DateTime(),
-		newProject.UpdatedAt,
-		newProject.StartDate,
-		newProject.DueDate,
-		"pending",
-		0,
-		newProject.Priority,
-		newProject.UserId,
-		newProject.Allocats,
-		0,
-		0,
-		new DateTime(),
-		newProject.IsPublic,
-		newProject.AllowBids,
-		newProject.Budget,
-		newProject.Currency,
-		newProject.Attachments
+    ProjectDto project = new(
+        "7f2c4b31-9d1a-4c71-b7ad-64b8e5f529d8",
+        "ALC-0011",
+        newProject.Title,
+        newProject.Description,
+        newProject.Type,
+        newProject.Category,
+        new DateTime(),
+        newProject.UpdatedAt,
+        newProject.StartDate,
+        newProject.DueDate,
+        "pending",
+        0,
+        newProject.Priority,
+        newProject.UserId,
+        newProject.Allocats,
+        0,
+        0,
+        new DateTime(),
+        newProject.IsPublic,
+        newProject.AllowBids,
+        newProject.Budget,
+        newProject.Currency,
+        newProject.Attachments
+    );
 
-	);
-
-	projects.Add(project);
+    projects.Add(project);
 });
 
 
