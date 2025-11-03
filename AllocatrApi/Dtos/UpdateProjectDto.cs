31620137392e64a3a9de366771ca91e68ae3f7ec
bytebytrevor <trevormngwenya@gmail.com>
2025-11-03 07:51:07 +0200

    Add Create and Update project

---
+++ AllocatrApi/Dtos/UpdateProjectDto.cs
@@ -0,0 +1,27 @@
+namespace AllocatrApi.Dtos;
+
+public record class UpdateProjectDto(
+    string Id,
+    string ProjectCode,
+    string Title,
+    string Description,
+    string Type,
+    string Category,
+    DateTime CreatedAt,
+    DateTime? UpdatedAt,
+    DateOnly? StartDate,
+    DateOnly? DueDate,
+    string Status,
+    int Progress,
+    string? Priority,
+    string UserId,
+    List<ProjectMemberDto>? Allocats,
+    int? TasksCount,
+    int? MessagesCount,
+    DateTime? LastActivity,
+    bool IsPublic,
+    bool AllowBids,
+    decimal? Budget,
+    string Currency,
+    string[] Attachments
+);
--- AllocatrApi/Program.cs
+++ AllocatrApi/Program.cs
@@ -14,40 +14,87 @@ app.MapGet("/", () => "Hello World!");
 app.MapGet("projects", () => projects);
 
 // GET /projects/1
-app.MapGet("projects/{id}", (string id) => projects.Find(project => project.Id == id));
+app.MapGet("projects/{id}", (string id) =>
+{
+	var project = projects.Find(project => project.Id == id);
+	if (project == null)
+		return Results.NotFound();
+
+	return Results.Ok(project);
+
+}).WithName("GetProject");
 
 // POST /projects
 app.MapPost("projects", (CreateProjectDto newProject) =>
 {
-    ProjectDto project = new(
-        "7f2c4b31-9d1a-4c71-b7ad-64b8e5f529d8",
-        "ALC-0011",
-        newProject.Title,
-        newProject.Description,
-        newProject.Type,
-        newProject.Category,
-        new DateTime(),
-        newProject.UpdatedAt,
-        newProject.StartDate,
-        newProject.DueDate,
-        "pending",
-        0,
-        newProject.Priority,
-        newProject.UserId,
-        newProject.Allocats,
-        0,
-        0,
-        new DateTime(),
-        newProject.IsPublic,
-        newProject.AllowBids,
-        newProject.Budget,
-        newProject.Currency,
-        newProject.Attachments
-    );
+	ProjectDto project = new(
+		"7f2c4b31-9d1a-4c71-b7ad-64b8e5f529d8",
+		"ALC-0012",
+		newProject.Title,
+		newProject.Description,
+		newProject.Type,
+		newProject.Category,
+		new DateTime(),
+		newProject.UpdatedAt,
+		newProject.StartDate,
+		newProject.DueDate,
+		"pending",
+		0,
+		newProject.Priority,
+		newProject.UserId,
+		newProject.Allocats,
+		0,
+		0,
+		new DateTime(),
+		newProject.IsPublic,
+		newProject.AllowBids,
+		newProject.Budget,
+		newProject.Currency,
+		newProject.Attachments
+	);
 
-    projects.Add(project);
+	projects.Add(project);
+
+	return Results.CreatedAtRoute("GetProject", new { id = project.Id}, project);
+});
+
+// POST /project/1
+app.MapPut("projects/{id}", (string id, UpdateProjectDto updatedProject) =>
+{
+	int index = projects.FindIndex(project => project.Id == id);
+
+	projects[index] = new ProjectDto(
+		updatedProject.Id,
+		updatedProject.ProjectCode,
+		updatedProject.Title,
+		updatedProject.Description,
+		updatedProject.Type,
+		updatedProject.Category,
+		updatedProject.CreatedAt,
+		updatedProject.UpdatedAt,
+		updatedProject.StartDate,
+		updatedProject.DueDate,
+		updatedProject.Status,
+		updatedProject.Progress,
+		updatedProject.Priority,
+		updatedProject.UserId,
+		updatedProject.Allocats,
+		updatedProject.TasksCount,
+		updatedProject.MessagesCount,
+		updatedProject.LastActivity,
+		updatedProject.IsPublic,
+		updatedProject.AllowBids,
+		updatedProject.Budget,
+		updatedProject.Currency,
+		updatedProject.Attachments
+	);
 });
 
 
+// DELETE /projects/1
+app.MapDelete("projects/{id}", (string id) =>
+{
+	projects.RemoveAll(project => project.Id.Equals(id));
+});
 
 app.Run();
